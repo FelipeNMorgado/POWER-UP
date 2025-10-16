52aa6bfca473c51c8ef0322e2bf5dc1c32446d85
FelipeNunes <felipe.nunes.morgado@gmail.com>
2025-10-16 17:17:41 -0300

    classes organization

--- dominio/src/main/java/Up/Power/rivalidade/RivalidadeService.java
+++ dominio/src/main/java/Up/Power/rivalidade/RivalidadeService.java
@@ -2,7 +2,9 @@ package Up.Power.rivalidade;
 
 import Up.Power.perfil.PerfilId;
 
-public interface RivalidadeService {
-    String resultado(PerfilId perfil1, PerfilId perfil2);
+public class RivalidadeService {
+    String resultado(PerfilId perfil1, PerfilId perfil2) {
+        return null;
+    }
 
 }
