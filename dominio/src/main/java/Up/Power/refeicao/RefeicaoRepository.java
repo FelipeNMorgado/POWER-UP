82950a5426d8f15dfae1a8154e886b3bae1b22b6
Lucas de Holanda <lhbs@cesar.school>
2025-10-18 19:41:41 -0300

    Atualizando o cml

--- dominio/src/main/java/Up/Power/refeicao/RefeicaoRepository.java
+++ dominio/src/main/java/Up/Power/refeicao/RefeicaoRepository.java
@@ -2,6 +2,12 @@ package Up.Power.refeicao;
 
 import Up.Power.Refeicao;
 
+import java.util.List;
+
 public interface RefeicaoRepository {
     void salvar(Refeicao refeicao);
+    void editar(RefeicaoId refeicao);
+    void excluir(RefeicaoId refeicao);
+    Refeicao obter(RefeicaoId refeicao);
+    List<Refeicao> listar(RefeicaoId refeicao);
 }
