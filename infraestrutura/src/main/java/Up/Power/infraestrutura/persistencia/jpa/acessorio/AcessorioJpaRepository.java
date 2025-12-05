c6711ada9f36bd4bb0f45ecab9face0dc55e1b1f
FelipeNunes <felipe.nunes.morgado@gmail.com>
2025-12-05 17:54:51 -0300

    Ajustes infra

--- infraestrutura/src/main/java/Up/Power/infraestrutura/persistencia/jpa/acessorio/AcessorioJpaRepository.java
+++
@@ -1,5 +0,0 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface AcessorioJpaRepository extends JpaRepository<AcessorioJpa, Integer> {
-    // Aqui você pode criar consultas personalizadas se quiser
-}
--- infraestrutura/src/main/java/Up/Power/infraestrutura/persistencia/jpa/avatar/AvatarJpaRepository.java
+++
@@ -1,5 +0,0 @@
-package Up.Power.infraestrutura.persistencia.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface AvatarJpaRepository extends JpaRepository<AvatarJpa, Integer> {}
